d6ce0cde88e6730356d4a1bb269cfd9f94356c2c
м <174154109+n11kol11c@users.noreply.github.com>
2025-11-04 22:23:32 +0100

    Update callbacks.rs

--- src/callbacks.rs
+++ src/callbacks.rs
@@ -1,5 +1,24 @@
-pub type CallBack<T><dyn Fn(T) + Send + Sync>;
+pub type Callback<T> = Box<dyn Fn(T) + Send + Sync>;
 
 pub struct CallbackHandler<T> {
     callbacks: Vec<Callback<T>>,
+}
+
+impl<T> CallbackHandler<T> {
+    pub fn new() -> Self {
+        Self { callbacks: vec![] }
+    }
+
+    pub fn register(&mut self, callback: Callback<T>) {
+        self.callbacks.push(callback);
+    }
+
+    pub fn trigger(&self, value: T)
+    where
+        T: Clone,
+    {
+        for cb in &self.callbacks {
+            cb(value.clone());
+        }
+    }
 }
