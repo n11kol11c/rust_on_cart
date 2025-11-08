53dab824247c92ed7e1a4b257f01749185f912f8
м <174154109+n11kol11c@users.noreply.github.com>
2025-11-08 16:52:41 +0100

    Update fs.rs

--- src/core/fs.rs
+++ src/core/fs.rs
@@ -4,9 +4,9 @@ use crate::errors::error::CartError;
 
 pub fn write_file(path: &str, content: &str) -> Result<(), CartError> {
     File::create(path)
-        .map_err(|e| CartError::IoError(e.to_string()))?
+        .map_err(|e: std::io::Error| CartError::IoError(e.to_string()))?
         .write_all(content.as_bytes())
-        .map_err(|e| CartError::IoError(e.to_string()))
+        .map_err(|e: std::io::Error| CartError::IoError(e.to_string()))
 }
 
 pub fn append_file(path: &str, content: &str) -> Result<(), CartError> {
@@ -14,18 +14,18 @@ pub fn append_file(path: &str, content: &str) -> Result<(), CartError> {
     OpenOptions::new()
         .append(true)
         .open(path)
-        .map_err(|e| CartError::IoError(e.to_string()))?
+        .map_err(|e: std::io::Error| CartError::IoError(e.to_string()))?
         .write_all(content.as_bytes())
-        .map_err(|e| CartError::IoError(e.to_string()))
+        .map_err(|e: std::io::Error| CartError::IoError(e.to_string()))
 }
 
 pub fn read_file(path: &str) -> Result<String, CartError> {
-    let mut file = File::open(path).map_err(|e| CartError::IoError(e.to_string()))?;
-    let mut content = String::new();
-    file.read_to_string(&mut content).map_err(|e| CartError::IoError(e.to_string()))?;
+    let mut file: File = File::open(path).map_err(|e: std::io::Error| CartError::IoError(e.to_string()))?;
+    let mut content: String = String::new();
+    file.read_to_string(&mut content).map_err(|e: std::io::Error| CartError::IoError(e.to_string()))?;
     Ok(content)
 }
 
 pub fn delete_file(path: &str) -> Result<(), CartError> {
-    fs::remove_file(path).map_err(|e| CartError::IoError(e.to_string()))
+    fs::remove_file(path).map_err(|e: std::io::Error| CartError::IoError(e.to_string()))
 }
