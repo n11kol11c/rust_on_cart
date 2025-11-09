8d156dd0bb9c04710d6c45c138460bc49fefeb4f
м <174154109+n11kol11c@users.noreply.github.com>
2025-11-09 19:34:52 +0100

    Update mod.rs

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -3,9 +3,11 @@ pub mod db;
 pub mod net;
 pub mod resource;
 pub mod utils;
+pub mod json;
 
 pub use fs::*;
 pub use db::*;
 pub use net::*;
 pub use resource::*;
 pub use utils::*;
+pub use json::*;
